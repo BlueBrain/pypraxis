64005a0fc9942492db85af58111b4d6b4af75114
Christoph Schmidt-Hieber <christsc@gmx.de>
2009-09-03 12:47:38 +0100

    Modified compiler script.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
 # This works on 32bit systems:
-f2py --fcompiler=g95 -c praxis.pyf praxis.f
+# f2py --fcompiler=g95 -c praxis.pyf praxis.f
+
+# This works on 32bit and 64bit systems:
+f2py --fcompiler=gnu95 --f77flags=-fomit-frame-pointer --noopt -c praxis.pyf praxis.f
 
 # This works on 64bit systems:
 # f2py --fcompiler=g95 --f90flags=-fpic --f77flags=-fpic -c praxis.pyf praxis.f
