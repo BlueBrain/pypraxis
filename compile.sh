c56940a563d186a5c7a5fc066642c73c382c573c
Christoph Schmidt-Hieber <christsc@gmx.de>
2009-09-03 12:07:06 +0100

    Added some test cases from scipy.optimize. Added a function wrapper to match existing scipy.optimize signatures.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# This works on 32bit systems:
+f2py --fcompiler=g95 -c praxis.pyf praxis.f
+
+# This works on 64bit systems:
 # f2py --fcompiler=g95 --f90flags=-fpic --f77flags=-fpic -c praxis.pyf praxis.f
-f2py --fcompiler=gfortran --noopt -c praxis.pyf praxis.f
+
+# This works on 32 and 64bit systems:
+# f2py --fcompiler=gfortran --noopt -c praxis.pyf praxis.f
 
---
+++ test.py
@@ -0,0 +1,136 @@
+import numpy as np
+from scipy.optimize import *
+from praxis import praxis
+
+def fmin_praxis(func, x0, args=(), ftol=1e-5, maxstep=1.0, disp=1):
+    """Minimize a function using Brent's principal axis method.
+
+    :Parameters:
+
+      func : callable f(x,*args)
+          Objective function to be minimized.
+      x0 : ndarray
+          Initial guess.
+      args : tuple
+          Extra arguments passed to func.
+
+    :Returns: (xopt, fopt)
+
+        xopt : ndarray
+            Parameter which minimizes `func`.
+
+    *Other Parameters*:
+
+      ftol : float
+          fmin_praxis attempts to return fopt sich that if x0 it the
+          true local minimum near xopt, then 
+          norm(x-x0) < ftol + sqrt(macheps)*norm(xopt)
+      maxstep : int
+          maxstep is the maximum step size. maxstep should be set to
+          about the maximum distance from the initial guess to the
+          minimum. If maxstep is set too large or too small, the
+          initial rate of convergence may be slow.
+      disp : int
+          controls the printing of intermediate results. Id disp=0,
+          nothing is printed. Will be more and more verbose up to
+          prin=4.
+      eps : float
+          eps is the machine precision.
+
+    """
+
+    eps = np.finfo(np.double).eps
+
+    fopt, xopt = praxis( t0=ftol, h0=maxstep, x=x0, f=func,
+                         f_extra_args = args, prin=disp, machep=eps)
+
+    return xopt, fopt
+
+def main():
+    import time
+
+    times = []
+    algor = []
+    x0 = [0.8,1.2,0.7]
+    print "Nelder-Mead Simplex"
+    print "==================="
+    start = time.time()
+    x = fmin(rosen,x0)
+    print x
+    times.append(time.time() - start)
+    algor.append('Nelder-Mead Simplex\t')
+
+    print
+    print "Powell Direction Set Method"
+    print "==========================="
+    start = time.time()
+    x = fmin_powell(rosen,x0)
+    print x
+    times.append(time.time() - start)
+    algor.append('Powell Direction Set Method.')
+
+    print
+    print "Brent Principal Axis Method"
+    print "==========================="
+    start = time.time()
+    x = fmin_praxis(rosen,x0)
+    print x
+    times.append(time.time() - start)
+    algor.append('Brent Principal Axis Method.')
+
+    print
+    print "Nonlinear CG"
+    print "============"
+    start = time.time()
+    x = fmin_cg(rosen, x0, fprime=rosen_der, maxiter=200)
+    print x
+    times.append(time.time() - start)
+    algor.append('Nonlinear CG     \t')
+
+    print
+    print "BFGS Quasi-Newton"
+    print "================="
+    start = time.time()
+    x = fmin_bfgs(rosen, x0, fprime=rosen_der, maxiter=80)
+    print x
+    times.append(time.time() - start)
+    algor.append('BFGS Quasi-Newton\t')
+
+    print
+    print "BFGS approximate gradient"
+    print "========================="
+    start = time.time()
+    x = fmin_bfgs(rosen, x0, gtol=1e-4, maxiter=100)
+    print x
+    times.append(time.time() - start)
+    algor.append('BFGS without gradient\t')
+
+
+    print
+    print "Newton-CG with Hessian product"
+    print "=============================="
+    start = time.time()
+    x = fmin_ncg(rosen, x0, rosen_der, fhess_p=rosen_hess_prod, maxiter=80)
+    print x
+    times.append(time.time() - start)
+    algor.append('Newton-CG with hessian product')
+
+
+    print
+    print "Newton-CG with full Hessian"
+    print "==========================="
+    start = time.time()
+    x = fmin_ncg(rosen, x0, rosen_der, fhess=rosen_hess, maxiter=80)
+    print x
+    times.append(time.time() - start)
+    algor.append('Newton-CG with full hessian')
+
+    print
+    print "\nMinimizing the Rosenbrock function of order 3\n"
+    print " Algorithm \t\t\t       Seconds"
+    print "===========\t\t\t      ========="
+    for k in range(len(algor)):
+        print algor[k], "\t -- ", times[k]
+
+if __name__ == "__main__":
+    main()
